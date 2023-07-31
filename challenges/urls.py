2512cc114bd40e614d28ba6c8180e9616740c6ad
RyanClayKao <f256987584@gmail.com>
2023-07-31 18:26:44 +0800

    學會使用動態路徑(Dynamic path)

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('january', views.january),
-    path('february', views.february)
+    # path('january', views.january),
+    # path('february', views.february),
+    # 可以用這種<>的方式，將<>中的參數傳給 views的函式，但是那裏需要有同名的參數才行！
+    path('<month>', views.monthly_challenges)
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 def january(request):
@@ -7,3 +7,15 @@ def january(request):
 
 def february(request):
     return HttpResponse("一個月不喝含糖飲料")
+
+
+def monthly_challenges(request, month):
+    challenge_text = None
+    if month == "january":
+        challenge_text = "一周運動兩次"
+    elif month == "february":
+        challenge_text = "一個月不喝含糖飲料"
+    else:
+        return HttpResponseNotFound("尚不支援輸入的月份")
+
+    return HttpResponse(challenge_text)
