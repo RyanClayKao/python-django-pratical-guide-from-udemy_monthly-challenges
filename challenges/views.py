b5e7492a752612457b5f60363ba500039a7121db
高楚鈞 <T10822758@live.yuntech.edu.tw>
2023-07-31 18:12:03 +0800

    學會使用基本Views和Url設定

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def january(request):
+    return HttpResponse("一周運動兩次")
+
+def february(request):
+    return HttpResponse("一個月不喝含糖飲料")
